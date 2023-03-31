7e795ea805574f97c0812de6ec7230f756a6d6b8
josed <josedavidmunozrestrepo@gmail.com>
2023-03-31 17:07:23 -0500

    naviation Complete

---
+++ app/src/main/java/com/example/horoscopeapp/presentation/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.horoscopeapp.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.horoscopeapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+
+        val navHotFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHotFragment.navController
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
